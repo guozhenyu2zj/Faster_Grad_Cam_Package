bb4fe0d36bf9641f95ab6369a9a95ffe121f61f8
guozhenyu2zj <guozhenyu2zj@gmail.com>
2023-02-27 09:49:56 +0800

    init project

--- faster_grad_cam/01_float32/00_image_to_npy.py
+++ faster_grad_cam/01_float32/00_image_to_npy.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os, glob
 import numpy as np
- 
+
 dataset = []
 
 files = glob.glob("*.JPG")
@@ -10,6 +10,6 @@ for file in files:
     image = image.convert("RGB")
     data = np.asarray(image)
     dataset.append(data)
- 
+
 dataset = np.array(dataset)
 np.save("janken_dataset", dataset)
